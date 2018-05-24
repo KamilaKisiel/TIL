af93956c907d8479b048dc6eccf98bf2aa6bfd3a
KamilaKisiel <kisiel.kamila@gmail.com>
2018-05-24 23:36:47 +0200

    code refactoring, extracting addList func out of the listener method

--- JavaScript/SomeList/script.js
+++ JavaScript/SomeList/script.js
@@ -13,15 +13,20 @@ function createListElement() {
 	ul.appendChild(li); //new li is added at the and of existing list		input.value = ""; //clear input after entering item to prevent double adding
 }
 
-// add event listener to enter new items into the list 
-button.addEventListener("click", function(){ //listens on buttons clicking
+function addListAfterClick() {
 	if(inputLength() > 0) { //do not add items from the epmty input//
-	createListElement();	
+		createListElement();	
 	}
-})
+}
 
-input.addEventListener("keypress", function(event) { //listens on enter key clicking
+function addListAfterKeypress(event) { //we calling an event inside the func, it's important to pass it as a parameter in func
 	if (inputLength() > 0 && event.keyCode === 13) { //add an item when input is not empty and when enter key is pressed
 		createListElement();
 	}
-})
+}
+
+// add event listener to enter new items into the list 
+button.addEventListener("click", addListAfterClick)//listens on buttons clicking
+
+
+input.addEventListener("keypress", addListAfterKeypress) //listens on enter key clicking
