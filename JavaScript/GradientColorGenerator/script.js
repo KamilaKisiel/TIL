1d2edd5f05bd980399a931073b4576b9a3808513
KamilaKisiel <kisiel.kamila@gmail.com>
2018-05-27 20:30:50 +0200

    random color generator

--- JavaScript/GradientColorGenerator/script.js
+++ JavaScript/GradientColorGenerator/script.js
@@ -2,6 +2,7 @@ let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
+let button = document.getElementById("random");
 
 function setGradient() {
 	body.style.background = 
@@ -14,7 +15,7 @@ function setGradient() {
 	css.textContent = body.style.background + ";" //update text to show which color has been choose from gradient generator
 }
 
-//set random background color
+//get random background color
 function getRandomColor() {
 	let letters = '0123456789ABCDEF'; //hexa colors are combination of those 16 characters
 	let color = '#'; //every color in hexa starts with #
@@ -24,7 +25,24 @@ function getRandomColor() {
 	return color;
 }
 
+//set random color
+function setRandomColor() {
+	let randomColor1 = getRandomColor();
+	let randomColor2 = getRandomColor();
+	color1.value = randomColor1;
+	color2.value = randomColor2;
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ randomColor1
+	+ ", " 
+	+ randomColor2 + ")";
+	
+	css.textContent = body.style.background + ";"
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", setRandomColor);
 
