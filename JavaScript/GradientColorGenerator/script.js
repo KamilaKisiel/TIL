5b7df2aae863fcdfb58c7b435ef8942f1642652b
KamilaKisiel <kisiel.kamila@gmail.com>
2018-05-27 19:16:02 +0200

    checking function correctness

--- JavaScript/GradientColorGenerator/script.js
+++ JavaScript/GradientColorGenerator/script.js
@@ -3,4 +3,11 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 
+color1.addEventListener("input", function(){
+	console.log(color1.value);
+})
+
+color2.addEventListener("input", function(){
+	console.log(color1.value);
+})
 
