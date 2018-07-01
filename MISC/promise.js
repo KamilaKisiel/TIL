394cd3d9b192cbdf174b3d9fce96cef7db5156ec
KamilaKisiel <kisiel.kamila@gmail.com>
2018-07-01 21:34:34 +0200

    .catch error - example 2nd

--- MISC/promise.js
+++ MISC/promise.js
@@ -18,9 +18,13 @@ promise
 // .catch
 
 promise
-    .then(result => result + '!')
-    .then(result2 => {
+    .then(result => {
         throw Error
+        return result + '!'
+    })
+    .then(result2 => {
         console.log(result2);
 })
 .catch(() => console.log('error!'))
+//.catch loges the error no matter 
+//where it happens in the chain of promise
