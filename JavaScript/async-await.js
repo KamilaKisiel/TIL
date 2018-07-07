3e904d8f5e33af91cf4998e7b06c681d9f224d27
KamilaKisiel <kisiel.kamila@gmail.com>
2018-07-07 20:01:30 +0200

    example with removal of fetch..then expression

--- JavaScript/async-await.js
+++ JavaScript/async-await.js
@@ -8,3 +8,29 @@ async function fetchStarships() {
     const data = await response.json();
     console.log(data);
 }  
+
+// updating function so there is no fetch...then fragment
+const urls = [
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/albums'
+  ]
+  
+  const getData = async function() {
+    const [ users, posts, albums ] = await Promise.all(urls.map(url =>
+        fetch(url).then(resp => resp.json())
+    ));
+    console.log('users', users);
+    console.log('posts', posts);
+    console.log('albums', albums);
+  }
+  //updated
+  const getData = async function(){
+      const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
+          const response = await fetch(url);
+          return response.json();
+      }));
+      console.log('users', users);
+      console.log('posts', posts);
+      console.log('albums', albums);
+  }
