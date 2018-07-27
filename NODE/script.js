1f0c1d0bd1f6f1d87d4c51398a73457bfce297ea
KamilaKisiel <kisiel.kamila@gmail.com>
2018-07-27 19:02:22 +0200

    scripted runned with setTimeout func

--- NODE/script.js
+++ NODE/script.js
@@ -1,8 +1,12 @@
 const a = 4;
 const b = 5;
-console.log(a +b);
+// console.log(a +b);
 
 //in terminal:
 //Admin@HP MINGW64 ~/TIL/NODE (master)
 //$ node script.js
 //9
+
+setTimeout(() => {
+    console.log(a +b);  
+},6000)
