497c0e82631e2731f95db78701ad7e8ba92435f7
KamilaKisiel <kisiel.kamila@gmail.com>
2018-07-28 21:05:38 +0200

    module exporting in node

--- NODE/script.js
+++ NODE/script.js
@@ -1,6 +1,8 @@
-const a = 4;
+const script2 = require('./script2.js');
+
+const a = script2.largeNumber;
 const b = 5;
-// console.log(a +b);
+console.log(a +b);
 
 //in terminal:
 //Admin@HP MINGW64 ~/TIL/NODE (master)
@@ -11,4 +13,4 @@ const b = 5;
 //     console.log(a +b);  
 // },6000)
 
-console.log(__dirname); // gives the path for file directory 
+// console.log(__dirname); // gives the path for file directory 
