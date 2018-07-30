63c3facc685d8fee85d14f8e526ec6daa8e0ea3d
KamilaKisiel <kisiel.kamila@gmail.com>
2018-07-30 21:29:23 +0200

    acquiring requests answers

--- NODE/server.js
+++ NODE/server.js
@@ -9,6 +9,10 @@ const http = require('http')
 //and go back to the terminal you'll see 'I hear you! thanks for the request'
 
 const server = http.createServer((request, response) => {
+    console.log('headers', request.headers)
+    console.log('method', request.method)
+    console.log('url', request.url)
+    
     response.setHeader('Content-Type', 'text/html');
     response.end('<h1>Hellloooo</h1>')
 })
