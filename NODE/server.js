fd8b966519fa2babec987005aaba21b55ab981ae
KamilaKisiel <kisiel.kamila@gmail.com>
2018-07-30 20:37:55 +0200

    listening to the response in server

--- NODE/server.js
+++ NODE/server.js
@@ -1,10 +1,16 @@
 const http = require('http')
 
-const server = http.createServer(() => {
-    console.log('I hear you! thanks for the request')
-})
+// const server = http.createServer(() => {
+//     console.log('I hear you! thanks for the request')
+// })
 
-server.listen(3000); //then on terminal run `node server.js`- 
+// server.listen(3000); //then on terminal run `node server.js`- 
 //it is going to look like it's hanging in there, but if you open localhost:3000
 //and go back to the terminal you'll see 'I hear you! thanks for the request'
 
+const server = http.createServer((request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.end('<h1>Hellloooo</h1>')
+})
+
+server.listen(3000);
