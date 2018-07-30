5b1676703da32a73bfda7be2f7f4433c593b5eef
KamilaKisiel <kisiel.kamila@gmail.com>
2018-07-30 21:42:55 +0200

    sending json files

--- NODE/server.js
+++ NODE/server.js
@@ -12,9 +12,13 @@ const server = http.createServer((request, response) => {
     console.log('headers', request.headers)
     console.log('method', request.method)
     console.log('url', request.url)
+    const user = {
+        name: 'John',
+        hobby: 'Skating'
+    }
     
-    response.setHeader('Content-Type', 'text/html');
-    response.end('<h1>Hellloooo</h1>')
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(user));
 })
 
 server.listen(3000);
