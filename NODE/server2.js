ed8447ab28219128382dd840554a8dce8f0215a4
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-02 20:42:14 +0200

    sending urlencoded using body parser

--- NODE/server2.js
+++ NODE/server2.js
@@ -1,13 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('<h1>hello</h1>') //middleware to work properly need to call for the third parameter 'next'
-    next();
-})
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.send("tested")
-})
+    res.send("getting root")
+});
+
+app.get('/profile', (req, res) => {
+    res.send("getting profile")
+});
+
+app.post('/profile', (req, res) => {
+    console.log(req.body)
+    const user = {
+        name: "Sally",
+        hobby: "soccer"
+    }
+    res.send(user)
+});
+
 app.listen(3000);
