c2a129f9a8f7f586a7ae5e46f68d0e751b6ed90f
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-02 21:25:47 +0200

    sending json using body parser

--- NODE/server2.js
+++ NODE/server2.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}))
-
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
     res.send("getting root")
 });
