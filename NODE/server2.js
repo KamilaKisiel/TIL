0b4bbe43c05f728974324a3f47bb115acb028a02
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-02 19:29:18 +0200

    building server-getting responses depending on rooting

--- NODE/server2.js
+++ NODE/server2.js
@@ -3,10 +3,9 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    const user = {
-        name: 'Sally',
-        hobby: 'soccer'
-    }
-    res.send(user)
+    res.send("getting root")
+})
+app.get('/profile', (req, res) => {
+    res.send("getting profile")
 })
 app.listen(3000);
