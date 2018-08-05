24bee434df0790789bc7b70cbeeafaa54185533d
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-05 16:31:02 +0200

    request types - req.params

--- NODE/server2.js
+++ NODE/server2.js
@@ -5,10 +5,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
-app.get('/', (req, res) => {
+app.get('/:id', (req, res) => {
     //console.log(req.query); //than type in localhost:3000/?name=Kamila&age=34, and nodemon gives us { name: 'Kamila', age: '34' }
     req.body//by using middleware bodyParser we can receive everything tha we want in a request body
-    console.log(req.header);//by get method we can send json within headers
+    //console.log(req.header);//by get method we can send json within headers
+    console.log(req.params); //sending param afetr slash - localhost:3000/1234 gives in nodemon { id: '1234' }
+
     res.send("getting root")
 });
 
