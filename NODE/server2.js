4fca13e25a6e35f504f64abb86d122900b187450
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-05 16:26:41 +0200

    request types - req.header

--- NODE/server2.js
+++ NODE/server2.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     //console.log(req.query); //than type in localhost:3000/?name=Kamila&age=34, and nodemon gives us { name: 'Kamila', age: '34' }
     req.body//by using middleware bodyParser we can receive everything tha we want in a request body
+    console.log(req.header);//by get method we can send json within headers
     res.send("getting root")
 });
 
