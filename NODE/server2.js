d5a780d56e5548cb8d1e71a9030ffd3ae4e5f6c3
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-05 16:23:02 +0200

    request types - req.body

--- NODE/server2.js
+++ NODE/server2.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
-    console.log(req.query); //than type in localhost:3000/?name=Kamila&age=34, and nodemon gives us { name: 'Kamila', age: '34' }
+    //console.log(req.query); //than type in localhost:3000/?name=Kamila&age=34, and nodemon gives us { name: 'Kamila', age: '34' }
+    req.body//by using middleware bodyParser we can receive everything tha we want in a request body
     res.send("getting root")
 });
 
