4cfd2ca5387f42ea9e0fd3112bbe78d6350ff589
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-05 16:01:32 +0200

    request types - req.query

--- NODE/server2.js
+++ NODE/server2.js
@@ -4,18 +4,11 @@ const bodyParser = require('body-parser')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
+    console.log(req.query); //than type in localhost:3000/?name=Kamila&age=34, and nodemon gives us { name: 'Kamila', age: '34' }
     res.send("getting root")
 });
 
-app.get('/profile', (req, res) => {
-    res.send("getting profile")
-});
-
-app.post('/profile', (req, res) => {
-    console.log(req.body)
-    res.send('Success')
-});
 
 app.listen(3000);
