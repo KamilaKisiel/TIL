5f85a41a754dde6e04d4a75cf6623d51dcd82389
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-06 21:00:30 +0200

    sending static file-html

--- NODE/server2.js
+++ NODE/server2.js
@@ -3,16 +3,9 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json());
-app.get('/:id', (req, res) => {
-    //console.log(req.query); //than type in localhost:3000/?name=Kamila&age=34, and nodemon gives us { name: 'Kamila', age: '34' }
-    //req.body//by using middleware bodyParser we can receive everything tha we want in a request body
-    //console.log(req.header);//by get method we can send json within headers
-    //console.log(req.params); //sending param afetr slash - localhost:3000/1234 gives in nodemon { id: '1234' }
-
-    res.status(404).send("not found")
-});
-
+app.use(express.static(__dirname + '/public'))
+// if we wont to sent static file like html we need to use above syntax
+// /public is the name of the folder where html is saved
+// accordingly css and js file added to html will upload automatically 
 
 app.listen(3000);
