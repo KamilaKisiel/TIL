b62fc886abd2f57a81692bd74c2119379c424f4a
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-06 20:53:00 +0200

    sending response status 404

--- NODE/server2.js
+++ NODE/server2.js
@@ -7,11 +7,11 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 app.get('/:id', (req, res) => {
     //console.log(req.query); //than type in localhost:3000/?name=Kamila&age=34, and nodemon gives us { name: 'Kamila', age: '34' }
-    req.body//by using middleware bodyParser we can receive everything tha we want in a request body
+    //req.body//by using middleware bodyParser we can receive everything tha we want in a request body
     //console.log(req.header);//by get method we can send json within headers
-    console.log(req.params); //sending param afetr slash - localhost:3000/1234 gives in nodemon { id: '1234' }
+    //console.log(req.params); //sending param afetr slash - localhost:3000/1234 gives in nodemon { id: '1234' }
 
-    res.send("getting root")
+    res.status(404).send("not found")
 });
 
 
